cdcae2c39bdd54c3d408f22be4c94385922d2bb7
Cristur <cristianoturri@gmail.com>
2024-04-15 17:54:08 +0200

    committino

--- Backend/m1/s1/G1/src/Main.java
+++ Backend/m1/s1/G1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,7 +9,7 @@ public class Main {
      String strConcatenata = concatena("mi piace il numero: ", 20);
      System.out.println(strConcatenata);
      String[] strConcatenata2 = concatena2(new String[]{"ciao", "mi", "chiamo", "Cristiano", "Turri"}, "we");
-     System.out.println(strConcatenata2);
+     System.out.println(Arrays.toString(strConcatenata2));
 
     }
 
@@ -21,7 +23,14 @@ public class Main {
         return stringaConcatenata;
     }
     public static String[] concatena2(String[] a, String b){
-        String[] array = new String[]{a[0], a[1], a[2], a[3], a[4]};
+        String[] array = new String[6];
+        array[0] = a[0];
+        array[1] = a[1];
+        array[2] = b;
+        array[3] = a[2];
+        array[4] = a[3];
+        array[5] = a[4];
+
        return array;
     }
 }
