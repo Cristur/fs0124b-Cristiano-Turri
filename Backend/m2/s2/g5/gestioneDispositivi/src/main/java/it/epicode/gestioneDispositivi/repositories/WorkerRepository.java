42753da754e720c86ed77db17824e70e488b75ab
Cristur <cristianoturri@gmail.com>
2024-05-24 12:07:24 +0200

    Committino

--- Backend/m2/s2/g5/gestioneDispositivi/src/main/java/it/epicode/gestioneDispositivi/repositories/WorkerRepository.java
+++ Backend/m2/s2/g5/gestioneDispositivi/src/main/java/it/epicode/gestioneDispositivi/repositories/WorkerRepository.java
@@ -1,8 +1,18 @@
 package it.epicode.gestioneDispositivi.repositories;
 
 import it.epicode.gestioneDispositivi.data.Worker;
+import org.hibernate.jdbc.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long>, PagingAndSortingRepository<Worker, Long> {
+
+    List<Worker> getWorkers();
+    Optional<Worker> getWorker(Long id);
+    Worker save(Worker worker);
+    Worker uodate(Long workerId, Worker worker);
+    Worker delete(Long workerId);
 }
